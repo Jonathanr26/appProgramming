54fa8a6c8e36c0b3cde15e7f6dc62c1519846022
Jonathanr26 <jrodriguez110@ucol.mx>
2022-09-01 14:56:48 -0500

    actividad 4

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Products from "./src/Products";
+import Products_add from "./src/Products_add";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+const MyStack = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Products"
+        component={Products}
+        options={{
+          title: "Product List",
+          headerTitleAlign: "center",
+        }}
+      />
+      <Stack.Screen name="Products_add" component={Products_add} />
+    </Stack.Navigator>
+  );
+};
+
+const Home = () => {
+  return (
+    <View style={styles.view}>
+      <Text>Inicio!!!</Text>
     </View>
   );
-}
+};
+
+const Config = () => {
+  return (
+    <View style={styles.view}>
+      <Text>Configuraciones de mi app!!!</Text>
+    </View>
+  );
+};
+
+const Tab = createBottomTabNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Products" component={MyStack} />
+        <Tab.Screen name="Config" component={Config} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 35,
+    backgroundColor: "#bcbcbc",
+  },
+  view: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+export default App;
