1077e36eaf392e806edf654fb2e49f50479cb139
Jonathan Rodriguez <54649236+Jonathanr26@users.noreply.github.com>
2022-10-03 17:23:18 -0500

    Update

--- src/authentication/firebase-config.js
+++ src/authentication/firebase-config.js
@@ -1,10 +1,10 @@
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyA5PhxNJ2Gr0fom3Q2TSI6Q9HCgjmkB2Og",
-  authDomain: "act5-948b2.firebaseapp.com",
-  databaseURL: "https://act5-948b2-default-rtdb.firebaseio.com",
-  projectId: "act5-948b2",
-  storageBucket: "act5-948b2.appspot.com",
-  messagingSenderId: "761390378804",
-  appId: "1:761390378804:web:a913ff790e4b8ef2003f61"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
